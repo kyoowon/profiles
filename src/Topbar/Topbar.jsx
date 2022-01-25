d1f4bfbd1873f4f7ada7f6aacbe0a3858c018b58
kyoowon <kyoowon95@gmail.com>
2022-01-25 16:46:11 +0900

    topbar menu completed

--- src/Topbar/Topbar.jsx
+++ src/Topbar/Topbar.jsx
@@ -4,17 +4,16 @@ import Brightness4Icon from '@mui/icons-material/Brightness4';
 
 export default function Topbar({ darkMode, setDarkMode }) {
     return (
-        <div className={"topBar " + (darkMode && "active")}>
+        <div className={"topBar " + (darkMode && "dark_active ")}>
             <div className="warpper">
                 <div className="left">
                     <a href="#intro" className="logo">kyulee</a>
                 </div>
                 <div className="menuContainer">
-                    <ul className="menu">
+                    <ul className="windowMenu">
                         <li><a href="#intro">intro</a></li>
                         <li><a href="#skill">skill</a></li>
                         <li><a href="#portfolio">portfolio</a></li>
-                        <li></li>
                     </ul>
                 </div>
                 <div className="right">
